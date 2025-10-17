805a5f6256a488c3f54b4633f8fd8440521d6ad9
@Martinsjuuh <jujuliana.martinsjmds13@gmail.com>
2025-10-17 18:00:52 -0300

    add o mundo 2

---
+++ python3-mundo2/ex038.py
@@ -0,0 +1,7 @@
+# Desafio 038
+
+#Escreva um programa que leia dois números interiros e compare-os, mostrando na tela uma mensagem:
+
+# - 0 primeiro valor é maior
+# - 0 segundo valor é maior
+# - Não existe valor maior, os dois são iguais
