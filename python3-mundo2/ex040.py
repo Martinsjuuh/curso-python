4127b5299124b4a6aaec06b37027c071b12b6793
@Martinsjuuh <jujuliana.martinsjmds13@gmail.com>
2025-10-20 13:14:01 -0300

    Update ex040.py

--- python3-mundo2/ex040.py
+++ python3-mundo2/ex040.py
@@ -1,1 +1,12 @@
 # Desafio 040
+
+#Crie um programa que leia duas notas de um aluno e calcule sua média, mostrando uma mensagem no final,de acordo com a média atingida:
+
+# - Média abaixo de 5.0: Reprovado
+# - Média entre 5.0 e 6.9: Recuperação
+# - Média 7.0 ou superior: Aprovado
+
+n1 = int(input('Primeira nota: '))
+n2 = int(input('Segunda nota: '))   
+media = n1 + n2 / 2
+print('A media é {}'.format(media))
