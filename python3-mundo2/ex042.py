da43ec94522a723b993bcf4bd74c31b52530feaa
@Martinsjuuh <jujuliana.martinsjmds13@gmail.com>
2025-10-24 14:58:40 -0300

    add soluções dos exercícios

--- python3-mundo2/ex042.py
+++ python3-mundo2/ex042.py
@@ -9,5 +9,7 @@
 r1 = float(input('Primeiro segmento: '))
 r2 = float(input('Segundo segmento: '))
 r3 = float(input('Terceiro segmento: '))
- 
-print('Os segmentos acima PODEM FORMA um triângulo ESCALENO!')
+if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
+    print('Os segmentos acima PODEM FORMAR triângulo! ')
+else:
+    print('Os segmentos acima NÃo PODEM FORMAR triângulo!')
