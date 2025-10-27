f476c4620f15de483b00d29ae8476634004f743f
@Martinsjuuh <jujuliana.martinsjmds13@gmail.com>
2025-10-27 17:07:00 -0300

    add soluções dos exercícios

---
+++ python3-mundo2/ex048.py
@@ -0,0 +1,6 @@
+# Desafio 048
+
+# faça um programa que calculee a soma entre todos os números ímpares que são múltiplos de três e que se encontram no intervalo de 1 até 500.
+
+for c in range (1, 500):
+    print(c)
